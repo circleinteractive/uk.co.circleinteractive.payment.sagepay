6f00ffad943787a9758c684477c2d27a386bc9e9
Circle Interactive <technical@circle-interactive.co.uk>
2013-11-28 14:42:23 +0000

    Update licence info

--- sagepay.php
+++ sagepay.php
@@ -4,8 +4,8 @@
  * Sagepay Extension for CiviCRM - Circle Interactive 2012
  * Author: andyw@circle
  *
- * Distributed under the GNU Affero General Public License, version 3
- * http://www.gnu.org/licenses/agpl-3.0.html 
+ * Distributed under GPL v2
+ * https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // Define logging level (0 = off, 4 = log everything)
