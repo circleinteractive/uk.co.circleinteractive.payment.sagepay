0825c7afd707b9769e68559b5ce20dcf9598c232
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2018-11-13 18:25:54 +0000

    split method deprecated in php 7. Using php_split instead.

--- custom/php/CRM/Core/Payment/Sagepay.php
+++ custom/php/CRM/Core/Payment/Sagepay.php
@@ -512,7 +512,7 @@ class CRM_Core_Payment_Sagepay extends CRM_Core_Payment {
         ]);
 
         // Send request and split response into name/value pairs
-        $response = split(chr(10), curl_exec($session));
+        $response = preg_split('/' . chr(10) . '/', curl_exec($session));
 
         // Check that a connection was made
         if (curl_error($session)){
